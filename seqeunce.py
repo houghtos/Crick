9519eb5057b5834915dd2f5f2d2287d851150d9e
houghtos <sbn.houghton@gmail.com>
2014-07-08 22:30:53 -0400

    blah

--- seqeunce.py
+++ seqeunce.py
@@ -20,29 +20,33 @@ class sequence:    #Created a 'class' named sequence
 #Function: Sets seq variable equal to s
 
     def set( self, s ): #"def" declares a function.  The name  of the function is "set". All functions in a class must contain ( self
-        self.seq = s #This sets the class variable seq equal to the variable s ??????????
+
+        self.seq = s.lower()
+        print (self.seq)
         return #ends the function set
 
 
 
 #This will run if value is given
-    def check_seq ( self, s = None ):
+    def check_seq ( self, s = None):
         if s == None:
-            s = self.seq
-
-        isOkay = 0 # Set default 0 to return FALSE.  Must satisfy if matches ==len(s)
+            s=self.seq
         matches = 0 #Has a value of +1 added
         nucleotides = "atcg"
-
-
         for i in range(0, len(s)): #For loop run as much times as the length of the string, "i" is introduced as a loop variable equal to 0 running to length of "s" which is equal to "seq" class variable from running dna.set()
             for j in range (0, len(nucleotides)): #For loop runs
                 if s[i] == nucleotides[j]: #If list (array) of variable s[i which is defined as 0 --- THIS IS THE LINE I DONT UNDERSTAND
                     matches=matches+1 #Add one to variable matches introduced above
 
-        if matches == len(s):
+        if matches == len(s)and len(s) > 0:
             isOkay=1
+
+
+        else:
+            isOkay=0
+
         return isOkay
+
 
 
 #--------------------------
@@ -56,7 +60,7 @@ if __name__ == '__main__':
 
 
 dna = sequence() #Define DNA as apart of the sequence class.  Can now use functions
-dna.set()  #Sets class variable seq to string "atcg"
-dna.check_seq()
+dna.set("ATCA")  #Sets class variable seq to string "atcg"
+print (dna.check_seq())
 
 
